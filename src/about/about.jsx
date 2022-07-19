b691c826df58244092987929dd60937d32c095c9
Regis Leandro Sebastiani <regisls@gmail.com>
2022-07-19 09:15:26 -0300

    first commit

---
+++ src/about/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageHeader from '../template/PageHeader';
+
+export default porps => (
+    <div>
+        <PageHeader name="Sobre" small="Que sistema" />
+
+        <p>Máquina de gerar tarefas</p>
+    </div>
+)
---
+++ src/todo/todo.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import PageHeader from "../template/PageHeader";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>
+                <PageHeader name="Tarefas" small="Lista de tarefas" />
+                <TodoForm />
+                <TodoList />
+            </div>
+        )
+    }
+}
